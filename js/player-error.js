e3a0f3d57a61f4595533b7727b60fb6c3a066821
Alvaro Samudio <alvarosamudio@protonmail.com>
2021-12-21 18:37:03 -0500

    fix errors

--- js/player-error.js
+++ js/player-error.js
@@ -6,7 +6,7 @@ var ErrorPlugin = Clappr.ContainerPlugin.extend({
   show: function() {
     var $ = Clappr.$
     this.hide();
-    var txt = (this.options.errorPlugin && this.options.errorPlugin.text) ? this.options.errorPlugin.text : 'IPTV Channel Encountered Error';
+    var txt = (this.options.errorPlugin && this.options.errorPlugin.text) ? this.options.errorPlugin.text : 'Error de canal';
     this._err = $('<div>')
       .css({
         'position': 'absolute',
@@ -26,7 +26,7 @@ var ErrorPlugin = Clappr.ContainerPlugin.extend({
         .css({
           'font-size': '200%',
         }))
-      .append($('<p>').html('Retry this channel in few minutes or report to your IPTV provider.')
+      .append($('<p>').html('Vuelva a intentar este canal en unos minutos o informe a su proveedor de IPTV.')
         .css({
           'font-size': '120%',
           'margin': '15px',
